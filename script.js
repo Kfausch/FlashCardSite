074eb0c4a2dc75540be29241f9314ab5b94a10b4
Kevin Fausch <54674691+Kfausch@users.noreply.github.com>
2025-03-14 08:29:00 -0500

    Update script.js
    Adding Security Acronyms

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let showDefinition = false;
 const sets = [
   { name: "Cybersecurity Terms", file: "definitions_cybersecurity.txt" },
   { name: "Networking Terms", file: "definitions_networking.txt" },
-  //{ name: "Default Set", file: "definitions.txt" } // If you still have a default definitions file
+  { name: "Default Set", file: "SecurityAcronyms.txt" },
 ];
 
 window.addEventListener('DOMContentLoaded', () => {
